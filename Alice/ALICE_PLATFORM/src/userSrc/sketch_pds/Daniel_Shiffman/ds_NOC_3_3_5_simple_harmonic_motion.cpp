f1a9292e0fdb0babdf0c38047611da78b073e496
Philipp Siedler <p.d.siedler@gmail.com>
2017-12-18 18:13:13 +0000

    change

---
+++ Alice/ALICE_PLATFORM/src/userSrc/sketch_pds/Daniel_Shiffman/ds_NOC_3_3_5_simple_harmonic_motion.cpp
@@ -0,0 +1,99 @@
+#define _MAIN_
+#ifdef _MAIN_
+
+#include "main.h"
+#include "Mover.h"
+#include "Attractor.h"
+
+///////// ----------------------------------------- model - view - controller (MVC) paradigm / pattern / template  ----------------- ////////////////////////////// 
+/////////////////////////// model  ///////////////////////////////////////////
+
+Mover m[5];
+
+float attractor_size = 3;
+
+Attractor a(vec(0, 10, 0), attractor_size);
+Attractor b(vec(10, -15, 0), attractor_size);
+Attractor c(vec(-15, -15, 0), attractor_size);
+
+vec gravity(0, -0.1, 0);
+vec wind(0.05, 0, 0);
+
+ostream & operator<<(ostream & os, vec & v) {
+	os << "(" << v.x << ", " << v.y << ", " << v.z << ")";
+	return os;
+}
+
+void applyGravity(Mover& _m) {
+	_m.applyForce(gravity);
+}
+
+void applyWind(Mover& _m) {
+	_m.applyForce(wind);
+}
+
+void applyAttraction(Mover& _m) {
+	vec force = a.attract(_m);
+	force += b.attract(_m);
+	force += c.attract(_m);
+	_m.applyForce(force);
+
+}
+
+void applyForces(Mover& _m) {
+	applyGravity(_m);
+	applyWind(_m);
+	applyAttraction(_m);
+}
+
+void setup()
+{
+
+}
+
+void update(int value)
+{
+	for (int i = 0; i < 5; i++) {
+		applyForces(m[i]);
+		m[i].update();
+	}
+
+	//m.edges();
+}
+
+
+/////////////////////////// view  ///////////////////////////////////////////
+
+void draw()
+{
+
+	for (Mover n : m) {
+		n.display();
+	}
+
+	a.display();
+	b.display();
+	c.display();
+
+	// background + grid
+	glLineWidth(1);
+	backGround(1);
+	glColor3d(0.75, 0.75, 0.75);
+	drawGrid(100);
+}
+
+/////////////////////////// control  ///////////////////////////////////////////
+
+void mousePress(int b, int state, int x, int y)
+{
+}
+
+void mouseMotion(int x, int y)
+{
+}
+
+void keyPress(unsigned char k, int xm, int ym)
+{
+}
+
+#endif // _MAIN_
